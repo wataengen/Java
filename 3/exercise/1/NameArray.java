549704b3bce71309a3f4f1ac786d33a1d20da53e
wataengen <wata69naka@gmail.com>
2023-11-06 14:39:31 +0900

    ライブラリ2

--- 3/exercise/1/NameArray.java
+++ 3/exercise/1/NameArray.java
@@ -2,15 +2,17 @@ public class NameArray {
     public static void main(String[] args) {
         String[] names = {"桜井", "田原", "中川", "中村"};
         String search = args[0];
-        boolean result = false;
+        // boolean result = false;
+        int i;
 
-        for(int i = 0; i < names.length; i++) {
+        for(i = 0; i < names.length; i++) {
             if(names[i].equals(search)) {
-                result = true;
+                // result = true;
+                break;
             }
         }
 
-        if(result){
+        if(i < names.length){
             System.out.println(search + "さんは含まれています");
         } else {
             System.out.println(search + "さんは含まれていません");
